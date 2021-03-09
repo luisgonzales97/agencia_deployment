58c0dbeebdbbce0dfe47fa02ad5f512d4d4bc06a
luisgonzales97 <luisaltlv2010@gmail.com>
2021-03-09 04:51:51 -0500

    jee

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ dotenv.config({ path: 'variables.env' });
 console.log(process.env.BD_HOST);
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+    loggin: false,
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'mysql',
